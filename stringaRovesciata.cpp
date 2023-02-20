c003a71e93277654d96ed8dab1080f5913d44a5f
RiccardoGatto <gatto.riccardo@gmail.com>
2023-02-20 12:05:24 +0100

    Modifiche

--- stringaRovesciata.cpp
+++ stringaRovesciata.cpp
@@ -2,12 +2,45 @@
 #include <string>
 using namespace std;
 
-string rovescia (string word){
-    string rov="";
-    for (int i=word.length()-1; i>=0; i--){
+string rovescia(string word)
+{
+    string rov = "";
+    for (int i = word.length() - 1; i >= 0; i--)
+    {
         rov += word[i];
     }
     return rov;
+}
+string rovescia2(string word)
+{
+    int y = word.length() - 1;
+    char app;
+    for (int x = 0; x <= y; x++)
+    {
+        app = word[x];
+        word[x] = word[y];
+        word[y] = app;
+        y--;
+    }
+    return word;
+}
+string rovesciaRic(string word, int i)
+{
+    if (i == -1)
+    {
+        return "";
+    }
+    else
+    {
+        if (i == 0)
+        {
+            return to_string(word[0]);
+        }
+        else
+        {
+            return word[i] + rovesciaRic(word, i--);
+        }
+    }
 }
 
 int main()
@@ -15,5 +48,6 @@ int main()
     string parola;
     cout << "Inserisci una parola: ";
     cin >> parola;
-    cout << "\nLa parola inserita, al contrario, e': " << rovescia(parola);
+    // cout << "\nLa parola inserita, al contrario, e': " << rovescia2(parola);
+    cout << "\nLa parola inserita, al contrario, e': " << rovesciaRic(parola, parola.length()-1);
 }
